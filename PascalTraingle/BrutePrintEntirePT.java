639fcdc83af78273a60dd78035f8b41e04685cad
Jasleen Kaur <161830401+Jasleen-Kaur-123@users.noreply.github.com>
2025-03-12 06:06:27 +0000

    initial

--- PascalTraingle/BrutePrintEntirePT.java
+++ PascalTraingle/BrutePrintEntirePT.java
@@ -2,25 +2,30 @@ import java.util.Arrays;
 import java.util.ArrayList;
 public class Main{
     public static void main(String[] args){
-    ArrayList<Integer> ans = new ArrayList<>();
-    int n= 5;
+    int n= 6;
     int c= 1;
+    ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+     ans = printPT(ans, n);
+        for (ArrayList<Integer> row : ans) {
+            System.out.println(row);
+        }
     }
-    public static ArrayList<Integer> printPT(ArrayList<Integer> ans,int n,int c){
+    public static ArrayList<ArrayList<Integer>> printPT(ArrayList<ArrayList<Integer>> ans,int n){
         for(int i=0;i<n;i++){
-                ArrayList<Integer> temp = new ArrayList<>();
-            for(int j=0;j<i;j++){
-                temp.add(ncr(n-1,c-1));
+            ArrayList<Integer> temp = new ArrayList<>();
+            for(int j=0;j<=i;j++){
+                temp.add(ncr(i,j));
             }
             ans.add(temp);
         }
         return ans;
     }
-    public static int ncr(int n,int c){
-        int res;
-        for(int i=0;i<n;i++){
+    public static int ncr(int n, int c){
+        int res = 1;
+        for(int i=0;i<c;i++){
             res = res * (n-i);
             res = res / (i+1);
         }
+    return res;
     }
 }
